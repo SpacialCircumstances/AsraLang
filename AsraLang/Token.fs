9215f3f15c1b7c83b5b61b296cb2bc6967ba02e5
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-04-17 18:09:13 +0200

    Add token types

---
+++ AsraLang/Token.fs
@@ -0,0 +1,27 @@
+﻿module Token
+
+type TokenType =
+    | FloatLiteral of float
+    | IntLiteral of int64
+    | StringLiteral of string
+    | Identifier of string
+    | Equal
+    | Dot
+    | Colon
+    | BlockOpen //[
+    | BlockClose //]
+    | LeftParen //(
+    | RightParen //)
+    | Comma
+    | Arrow //->
+    | Comment
+
+type Position = {
+    line: int
+    column: int
+}
+
+type Token = {
+    token: TokenType
+    position: Position
+}
