e92e752f8c8402e42d863df311b99e3f540e3d58
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-04-17 17:55:20 +0200

    Add types for parsed syntax tree

---
+++ AsraLang/ParseTree.fs
@@ -0,0 +1,38 @@
+﻿module ParseTree
+
+type Literal = 
+    | StringLiteral of string
+    | IntLiteral of int64
+    | FloatLiteral of float
+
+type TypeAnnotated = {
+    typeName: string
+    varName: string
+}
+
+type Declaration = 
+    | Simple of string
+    | Annotated of TypeAnnotated
+
+type VariableDefinition = {
+    variableName: Declaration
+    value: Expression
+}
+
+and FunctionCall = 
+    | Unary of string
+    | Binary of string * Expression
+    | Keyword of (string * Expression) list
+
+and Block = {
+    parameters: (Declaration list) option
+    body: Expression list
+}
+
+and Expression =
+    | LiteralExpression of Literal
+    | DefineVariableExpression of VariableDefinition
+    | FunctionCallExpression of FunctionCall
+    | VariableExpression of string
+    | BlockExpression of Block
+    | GroupExpression of Expression
