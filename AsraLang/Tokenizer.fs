1f9520b0950d3c6cdfc9a3a9e2e522e7f5754eeb
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-04-17 22:04:33 +0200

    Parse single character tokens

--- AsraLang/Tokenizer.fs
+++ AsraLang/Tokenizer.fs
@@ -11,19 +11,47 @@ type State = {
     col: int
 }
 
+let singleCharTokens = dict [
+    '.', Dot
+    ',', Comma
+    '=', Equal
+    ':', Colon
+    '[', BlockOpen
+    ']', BlockClose
+    '(', LeftParen
+    ')', RightParen
+]
+
+let keywords = dict [
+    "->", Arrow
+]
+
 let hasEnded (state: State) = state.current >= state.source.Length
 
 let advance (state: State) = ({ state with current = state.current + 1 }, state.source.[state.current])
+
+let peek (state: State) = match hasEnded state with
+                            | true -> char 0
+                            | false -> state.source.[state.current]
 
 let skipWhitespace (state: State) =
     let sourceAfterCurrent = Seq.skip state.current state.source
     let nextNonWs = Seq.findIndex (fun c -> not (Char.IsWhiteSpace c)) sourceAfterCurrent + state.current
     { state with start = nextNonWs; current = nextNonWs }
 
-let rec nextToken (state: State) =
-    let state, nextChar = state |> skipWhitespace |> advance //Get first character after whitespace
-    match nextChar with
-        | _ -> ({ token = Comma; position = state.start }, state)
+let makeToken (state: State) (t: TokenType) = ({ token = t; position = state.start }, state)
+
+let nextToken (state: State) =
+    let state, curr = state |> skipWhitespace |> advance //Get first character after whitespace
+    let nextChar = peek state
+    match Char.IsWhiteSpace nextChar with
+        | true -> //Single char token
+            match singleCharTokens.ContainsKey curr with
+                | true -> makeToken state (singleCharTokens.Item curr)
+                | false -> makeToken state (Identifier (string curr))
+        | false ->
+            match curr with
+                | _ -> ({ token = Comment; position = state.start }, state)
 
 let tokenize (code: string): Token seq =
     let init = {
