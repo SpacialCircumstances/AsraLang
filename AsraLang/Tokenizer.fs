ce36367a6769b68325fedd64a9bd933f763b4c61
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-04-17 22:12:06 +0200

    Comment tokenization

--- AsraLang/Tokenizer.fs
+++ AsraLang/Tokenizer.fs
@@ -34,12 +34,19 @@ let peek (state: State) = match hasEnded state with
                             | true -> char 0
                             | false -> state.source.[state.current]
 
+let findNext (source: string) (start: int) (pred: char -> bool) =
+    let afterStart = Seq.skip start source
+    Seq.findIndex pred afterStart + start
+
 let skipWhitespace (state: State) =
-    let sourceAfterCurrent = Seq.skip state.current state.source
-    let nextNonWs = Seq.findIndex (fun c -> not (Char.IsWhiteSpace c)) sourceAfterCurrent + state.current
+    let nextNonWs = findNext state.source state.current (fun c -> not (Char.IsWhiteSpace c))
     { state with start = nextNonWs; current = nextNonWs }
 
 let makeToken (state: State) (t: TokenType) = ({ token = t; position = state.start }, state)
+
+let comment (state: State) =
+    let nextLinebreak = findNext state.source state.current (fun c -> c = '\n')
+    { token = Comment; position = state.start }, { state with start = nextLinebreak; current = nextLinebreak }
 
 let nextToken (state: State) =
     let state, curr = state |> skipWhitespace |> advance //Get first character after whitespace
@@ -51,6 +58,7 @@ let nextToken (state: State) =
                 | false -> makeToken state (Identifier (string curr))
         | false ->
             match curr with
+                | '#' -> comment state
                 | _ -> ({ token = Comment; position = state.start }, state)
 
 let tokenize (code: string): Token seq =
