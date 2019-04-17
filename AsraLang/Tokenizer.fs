1e8fe0dad77ea8d9bb282b6246ca0743b92dac56
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-04-17 18:21:57 +0200

    Add tokenizer state

--- AsraLang/Tokenizer.fs
+++ AsraLang/Tokenizer.fs
@@ -1,2 +1,21 @@
 ﻿module Tokenizer
 
+open Token
+
+type State = {
+    source: string
+    start: int
+    current: int
+    line: int
+    col: int
+}
+
+let tokenize (code: string): Token seq =
+    let init = {
+        source = code;
+        start = 0;
+        current = 0;
+        line = 1;
+        col = 1;
+    }
+    Seq.unfold (fun state -> None) init
