b39f7a82b7616428a43044d6d04191629036fb58
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-04-18 22:17:46 +0200

    Add test for single char tokens

--- AsraLang.Test/Tokenizer.fs
+++ AsraLang.Test/Tokenizer.fs
@@ -3,10 +3,29 @@ module Tokenizer
 open System
 open Xunit
 open Tokenizer
+open Token
+
+let tokensMatch (expected: TokenType seq) (got: Token seq) =
+    if Seq.length expected <> Seq.length got then
+        false
+    else
+        Seq.zip expected got |> Seq.forall (fun (e, gt) -> e = gt.token)
 
 [<Fact>]
-let ``Basic Tokenization`` () =
-    let input = " .   . .  .."
-    let tokens = tokenize input |> Seq.toList
-    printfn "%A" tokens
-    ()
+let ``Single char tokens`` () =
+    let input = ". : () [ ][] - > + / *"
+    let tokens = tokenize input
+    let expected = [
+        Dot
+        Colon
+        BlockOpen
+        BlockClose
+        BlockOpen
+        BlockClose
+        Identifier("-")
+        Identifier(">")
+        Identifier("+")
+        Identifier("/")
+        Identifier("*")
+    ]
+    Assert.True (tokensMatch expected tokens)
