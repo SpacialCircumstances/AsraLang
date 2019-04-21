8c06062e124760d437c05753eb123234ddf370d2
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-04-21 18:40:41 +0200

    Parse string literal with pidgin

--- AsraLang/Parser.fs
+++ AsraLang/Parser.fs
@@ -1,2 +1,19 @@
 ﻿module Parser
 
+open Pidgin
+open Token
+open System
+
+let token (f: Token -> bool) = Parser<Token>.Token(Func<Token, bool>(f))
+
+let map (p: Parser<Token, Token>) (f: Token -> 'a) = p.Select(Func<Token, 'a>(f))
+
+let (<!>) = map
+
+let parseStringLiteral = token (fun t ->
+                                    match t.token with
+                                        | StringLiteral _ -> true
+                                        | _ -> false) <!> (fun l -> 
+                                            match l.token with
+                                                | StringLiteral s -> ParseTree.StringLiteral s
+                                                | _ -> invalidOp "Error")
