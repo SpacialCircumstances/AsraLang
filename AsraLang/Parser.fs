f156b32af1ef062246eb1bd7867c3a50d79ecc30
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-04-22 14:19:42 +0200

    Parse literals

--- AsraLang/Parser.fs
+++ AsraLang/Parser.fs
@@ -9,9 +9,27 @@ let token (f: Token -> 'a option) = //TODO: Make more efficient, do not run f tw
                                                         | None -> false
                                                         | Some _ -> true)).Select(Func<Token, 'a>(fun t -> match f t with
                                                                                                             | Some a -> a
-                                                                                                            | None -> invalidOp "Expected token"))
+                                                                                                            | None -> invalidOp "Can never happen"))
+
+let choice (parsers: Parser<Token, 'a> seq) = Parser.OneOf parsers
 
 let parseStringLiteral = token (fun t ->
                                     match t.token with
                                         | StringLiteral str -> Some (ParseTree.StringLiteral str)
                                         | _ -> None)
+
+let parseIntLiteral = token (fun t ->
+                                match t.token with
+                                    | IntLiteral i -> Some (ParseTree.IntLiteral i)
+                                    | _ -> None)
+
+let parseFloatLiteral = token (fun t ->
+                                match t.token with
+                                    | FloatLiteral f -> Some (ParseTree.FloatLiteral f)
+                                    | _ -> None)
+
+let parseLiteral = choice [
+    parseStringLiteral
+    parseFloatLiteral
+    parseIntLiteral
+]
