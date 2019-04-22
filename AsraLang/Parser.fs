28972fd14f9d3ba96e9ac5770faeb61de501140a
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-04-22 14:24:10 +0200

    Parse programs consisting of literal expressions

--- AsraLang/Parser.fs
+++ AsraLang/Parser.fs
@@ -13,6 +13,12 @@ let token (f: Token -> 'a option) = //TODO: Make more efficient, do not run f tw
 
 let choice (parsers: Parser<Token, 'a> seq) = Parser.OneOf parsers
 
+let map (parser: Parser<Token, 'a>) (mapper: 'a -> 'b) = parser.Select(Func<'a, 'b>(mapper))
+
+let (<!>) = map
+
+let many1 (parser: Parser<Token, 'a>) = parser.AtLeastOnce()
+
 let parseStringLiteral = token (fun t ->
                                     match t.token with
                                         | StringLiteral str -> Some (ParseTree.StringLiteral str)
@@ -28,8 +34,8 @@ let parseFloatLiteral = token (fun t ->
                                     | FloatLiteral f -> Some (ParseTree.FloatLiteral f)
                                     | _ -> None)
 
-let parseLiteral = choice [
-    parseStringLiteral
-    parseFloatLiteral
-    parseIntLiteral
-]
+let parseLiteralExpression = choice [ parseStringLiteral; parseFloatLiteral; parseIntLiteral ] <!> ParseTree.LiteralExpression
+
+let parseExpression = choice [ parseLiteralExpression ]
+
+let parseProgram = many1 parseExpression
