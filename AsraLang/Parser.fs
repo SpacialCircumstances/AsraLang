55fe50a1d161febbdc1f4a4bd90c7d25577ff36e
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-04-23 15:19:04 +0200

    Backtrack for variable definitions

--- AsraLang/Parser.fs
+++ AsraLang/Parser.fs
@@ -19,6 +19,8 @@ let map (parser: Parser<Token, 'a>) (mapper: 'a -> 'b) = parser.Select(Func<'a, 
 
 let (<!>) = map
 
+let label (lbl: string) (parser: Parser<Token, 'a>) = parser.Labelled lbl
+
 let map2 (parser1: Parser<Token, 'a>) (parser2: Parser<Token, 'b>) (mapper: 'a -> 'b -> 'c) = Parser.Map(Func<'a, 'b, 'c>(mapper), parser1, parser2)
 
 let sepBy (parser: Parser<Token, 'a>) (sep: Parser<Token, 'b>) = parser.SeparatedAtLeastOnce sep
@@ -40,7 +42,7 @@ let parseFloatLiteral = token (fun t ->
                                     | FloatLiteral f -> Some (ParseTree.FloatLiteral f)
                                     | _ -> None)
 
-let parseLiteralExpression = choice [ parseStringLiteral; parseFloatLiteral; parseIntLiteral ] <!> ParseTree.LiteralExpression
+let parseLiteralExpression = choice [ parseStringLiteral; parseFloatLiteral; parseIntLiteral ] <!> ParseTree.LiteralExpression |> label "Literal"
 
 let parseIdentifier = token (fun t -> match t.token with
                                                 | Identifier i -> Some i
@@ -66,7 +68,7 @@ let parseSimpleDeclaration = parseIdentifier <!> ParseTree.Simple
 
 let parseDeclaration = parseSimpleDeclaration //TODO: Declaration with type annotation
 
-let parseVariableDefinitionExpression = map2 (parseDeclaration.Before(parseEqual)) (prec (fun () -> parseValueExpression)) (fun d e -> ParseTree.DefineVariableExpression { variableName = d; value = e })
+let parseVariableDefinitionExpression = Parser.Try (map2 (parseDeclaration.Before(parseEqual)) (prec (fun () -> parseValueExpression)) (fun d e -> ParseTree.DefineVariableExpression { variableName = d; value = e })) |> label "Variable definition"
 
 parseValueExpression <- choice [
     parseLiteralExpression
