e8dbd3905e1c9087afe9469d45576964d60b6db0
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-05-02 20:53:21 +0200

    Typecheck literals

---
+++ AsraLang/Typechecker.fs
@@ -0,0 +1,30 @@
+﻿module Typechecker
+
+module U = UntypedAST
+module T = TypedAST
+
+type Error = {
+    message: string
+}
+
+type State = {
+    context: Map<string, T.AType>
+    errors: Error list
+}
+
+let rec typeExpr (state: State) (expr: U.Expression) =
+    match expr with
+        | U.LiteralExpression lit ->
+            let literal, litType = match lit with
+                                    | U.StringLiteral str ->
+                                        T.String str, T.Native "String"
+                                    | U.IntLiteral i ->
+                                        T.Int i, T.Native "Int"
+                                    | U.FloatLiteral f ->
+                                        T.Float f, T.Native "Float"
+            T.LiteralExpression { atype = litType; literalValue = literal }, state
+        | _ -> raise(System.NotImplementedException())
+
+let typecheck (program: U.Expression seq) =
+    let init = { context = Map.empty; errors = [] }
+    Seq.mapFold typeExpr init program
