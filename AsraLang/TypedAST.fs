18bea8d58e839a97b4831c866c6d16581ac26092
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-05-02 17:54:19 +0200

    Add typed AST

---
+++ AsraLang/TypedAST.fs
@@ -0,0 +1,45 @@
+﻿module TypedAST
+
+type FunType = {
+    input: AType
+    output: AType
+}
+
+and AType = 
+    | Native of string
+    | FunctionType of FunType
+
+type LiteralValue = 
+    | Int of int64
+    | String of string
+    | Float of float
+
+type Literal = {
+    atype: AType
+    literalValue: LiteralValue
+}
+
+type VariableBinding = {
+    varName: string
+    varType: AType
+    value: Expression
+}
+
+and FunctionCall = {
+    func: Expression
+    funcType: AType
+    args: (Expression * AType) list
+}
+
+and Block = {
+    parameters: (Expression * AType) list
+    body: (Expression * AType) list
+}
+
+and Expression =
+    | LiteralExpression of Literal
+    | VariableBindingExpression of VariableBinding
+    | FunctionCallExpression of FunctionCall
+    | VariableExpression of string * AType
+    | BlockExpression of Block
+    | GroupExpression of Expression
