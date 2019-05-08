76c1614a8f3c3438fb846eab2ed1ec8f9bef35ec
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-05-08 17:15:40 +0200

    Add test for type printing

---
+++ AsraLang.Test/Types.fs
@@ -0,0 +1,34 @@
+﻿module Types
+
+open TypedAST
+open Xunit
+
+[<Fact>]
+let ``Type printing`` () =
+    let t1 = Native "String"
+    let t2 = Native "Unit"
+    let t3 = Native "Int"
+    Assert.Equal("String", t1.ToString())
+    Assert.Equal("Unit", t2.ToString())
+    Assert.Equal("Int", t3.ToString())
+    let ft1 = FunctionType {
+        input = t1;
+        output = t3;
+    }
+    Assert.Equal("String -> Int", ft1.ToString())
+    let ft2 = FunctionType {
+        input = t2;
+        output = t3;
+    }
+    Assert.Equal("Unit -> Int", ft2.ToString())
+    let ft3 = FunctionType {
+        input = t1;
+        output = ft2;
+    }
+    Assert.Equal("String -> Unit -> Int", ft3.ToString())
+    let ft4 = FunctionType {
+        input = ft1;
+        output = t2;
+    }
+    Assert.Equal("(String -> Int) -> Unit", ft4.ToString())
+    ()
