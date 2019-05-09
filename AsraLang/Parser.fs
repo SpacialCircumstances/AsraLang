40841d7088678e4a5e8fb57826be820c8efafe8b
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-05-09 17:32:50 +0200

    Parse number literals

--- AsraLang/Parser.fs
+++ AsraLang/Parser.fs
@@ -2,11 +2,36 @@
 
 open FParsec
 open UntypedAST
+open System
 
-let expressionParser = anyString 2 |>> fun _ -> LiteralExpression (StringLiteral "Test")
+let floatLiteralParser: Parser<Literal, unit> = numberLiteral (NumberLiteralOptions.DefaultFloat) "Float literal" |>> fun f -> 
+    match f.IsInteger with
+        | true -> int64 f.String |> IntLiteral
+        | false -> float f.String |> FloatLiteral
 
-let programParser = many expressionParser
+let intLiteralParser = pint64 |>> IntLiteral
+
+let quoteParser = skipChar '"'
+
+let stringLiteralParser = quoteParser >>. (manyCharsTill anyChar quoteParser) |>> (fun s -> StringLiteral s)
+
+let literalParser = choice [
+    stringLiteralParser 
+    floatLiteralParser
+    intLiteralParser ] |>> (fun lit -> LiteralExpression lit)
+
+let separatorParser = skipMany1 (skipChar '\n' <|> skipChar ';')
+
+let ws = skipMany (pchar ' ' <|> pchar '\t')
+
+let singleExpressionParser = choice [
+    literalParser
+]
+
+let expressionParser = ws >>. singleExpressionParser .>> ws
+
+let programParser = sepEndBy expressionParser separatorParser .>> eof
 
 let parse (code: string) = match CharParsers.run programParser code with
                                 | Success (res, a, b) -> Result.Ok res
-                                | Failure (es, _, _) -> Result.Error es
+                                | Failure (es, e, _) -> Result.Error e
