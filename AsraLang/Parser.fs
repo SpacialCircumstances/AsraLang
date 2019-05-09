74a291cf462dd15c56d7ba2cdfbe7a42f3581b02
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-05-09 17:50:41 +0200

    Parse variable expressions

--- AsraLang/Parser.fs
+++ AsraLang/Parser.fs
@@ -4,6 +4,16 @@ open FParsec
 open UntypedAST
 open System
 
+let isSeparator (c: char) = Char.IsWhiteSpace c || c = ';' || c = '(' || c = ')' || c = '[' || c = ']' || c = ',' || c = ':' || c = '#'
+
+let isIdentifierStart (c: char) = (not (isDigit c)) && not (isSeparator c)
+
+let isIdentifierContinue (c: char) = not (isSeparator c)
+
+let identifierOptions = IdentifierOptions(isAsciiIdStart = isIdentifierStart, isAsciiIdContinue = isIdentifierContinue)
+
+let identifierParser: Parser<string, unit> = identifier identifierOptions
+
 let floatLiteralParser: Parser<Literal, unit> = numberLiteral (NumberLiteralOptions.DefaultFloat) "Float literal" |>> fun f -> 
     match f.IsInteger with
         | true -> int64 f.String |> IntLiteral
@@ -15,17 +25,17 @@ let quoteParser = skipChar '"'
 
 let stringLiteralParser = quoteParser >>. (manyCharsTill anyChar quoteParser) |>> (fun s -> StringLiteral s)
 
-let literalParser = choice [
-    stringLiteralParser 
-    floatLiteralParser
-    intLiteralParser ] |>> (fun lit -> LiteralExpression lit)
+let literalExpressionParser = choice [ stringLiteralParser; floatLiteralParser; intLiteralParser ] |>> (fun lit -> LiteralExpression lit)
+
+let variableExpressionParser = identifierParser |>> VariableExpression
 
 let separatorParser = skipMany1 (skipChar '\n' <|> skipChar ';')
 
 let ws = skipMany (pchar ' ' <|> pchar '\t')
 
 let singleExpressionParser = choice [
-    literalParser
+    literalExpressionParser
+    variableExpressionParser
 ]
 
 let expressionParser = ws >>. singleExpressionParser .>> ws
