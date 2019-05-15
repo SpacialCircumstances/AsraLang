4e79da7fd428e82e500e289896f9ac290712502b
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-05-15 22:41:36 +0200

    Generation of very basic js consisting of assignment and literals

--- AsraLang/JsGenerator.fs
+++ AsraLang/JsGenerator.fs
@@ -3,6 +3,7 @@
 open System.Text
 open TypedAST
 open Typechecker
+open System
 
 type GenerationState = {
     externFunctionsMapper: Map<string, string>
@@ -14,8 +15,20 @@ let genState (prelude: string) (externs: Extern list) = {
     externFunctionsMapper = Map.ofList (List.map (fun ext -> ext.asraName, ext.externName) externs)
 }
 
-let writeJs (state: GenerationState) (writer: StringBuilder) (expr: Expression) =
-    writer
+let rec writeJs (state: GenerationState) (writer: StringBuilder) (expr: Expression) =
+    match expr with
+        | VariableBindingExpression binding ->
+            do writer.Append (sprintf "let %s = " binding.varName) |> ignore
+            do writeJs state writer binding.value |> ignore
+            do writer.AppendLine ";" |> ignore
+            writer
+        | LiteralExpression lit ->
+            match lit.literalValue with
+                | LiteralValue.String str -> writer.Append(sprintf "\"%s\"" str)
+                | LiteralValue.Int i -> writer.Append i
+                | LiteralValue.Float f -> writer.Append f
+
+        | _ -> raise (NotImplementedException())
 
 let generateJs (state: GenerationState) (ast: Expression seq) =
     let sb = StringBuilder().Append(state.prelude)
