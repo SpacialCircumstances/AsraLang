e9258d25581bff2df0d72a901427d56bc1011da4
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-05-16 20:16:03 +0200

    Generate JS code for function calls

--- AsraLang/JsGenerator.fs
+++ AsraLang/JsGenerator.fs
@@ -16,6 +16,14 @@ let genState (prelude: string) (externs: Extern list) = {
     externFunctionsMapper = Map.ofList (List.map (fun ext -> ext.asraName, ext.externName) externs)
 }
 
+let rec private writeArguments (writeJs: StringBuilder -> Expression -> StringBuilder) (writer: StringBuilder) (arg: Expression) (rest: Expression list) =
+    writeJs writer arg |> ignore 
+    match List.tryHead rest with
+        | None -> ()
+        | Some head ->
+            writer.Append(", ") |> ignore
+            writeArguments writeJs writer head (List.tail rest)
+
 let rec writeJs (state: GenerationState) (writer: StringBuilder) (expr: Expression) =
     match expr with
         | VariableBindingExpression binding ->
@@ -36,6 +44,21 @@ let rec writeJs (state: GenerationState) (writer: StringBuilder) (expr: Expressi
             writer.Append("(") |> ignore
             writeJs state writer expr |> ignore
             writer.Append(")")
+        | FunctionCallExpression funCall ->
+            match funCall.func with
+                | VariableExpression _ ->
+                    writeJs state writer funCall.func |> ignore
+                | BlockExpression _ ->
+                    writer.Append "(" |> ignore
+                    writeJs state writer funCall.func |> ignore
+                    writer.Append ")" |> ignore
+                | _ -> invalidOp "Invalid expression for function call"
+            writer.Append "(" |> ignore
+            let args = List.map (fun (e, _) -> e) funCall.args
+            if not (List.isEmpty args) then
+                writeArguments (writeJs state) writer (List.head args) (List.tail args)
+            else ()
+            writer.AppendLine ");"
         | _ -> raise (NotImplementedException())
 
 let generateJs (state: GenerationState) (ast: Expression seq) =
