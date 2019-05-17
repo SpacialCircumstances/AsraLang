d9804464a1844292f1b5393d048cc17ba83ed6ac
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-05-17 14:02:07 +0200

    Make prelude use curried functions

--- AsraLang.Compiler/Prelude/prelude.js
+++ AsraLang.Compiler/Prelude/prelude.js
@@ -1,19 +1,5 @@
-﻿function println(str) {
-    console.log(str);
-}
-
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
+﻿const println = console.log;
+const add = (a) => (b) => a + b;
+const subtract = (a) => (b) => a - b;
+const multiply = (a) => (b) => a * b;
+const divide = (a) => (b) => a / b;
