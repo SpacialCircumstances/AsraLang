e682b0b2f2526835bc66644d2e367f484568ffa1
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-06-03 19:15:53 +0200

    Find main and output file in integration test and assert their presence

--- AsraLang.IntegrationTests/Tests.fs
+++ AsraLang.IntegrationTests/Tests.fs
@@ -4,6 +4,8 @@ open System.IO
 open Xunit
 
 let integrationTestDirectory = "../../../../Tests"
+let mainFileName = "main.asra"
+let outputFileName = "output.txt"
 
 let getTestCases () =
     if Directory.Exists integrationTestDirectory then
@@ -14,5 +16,8 @@ let getTestCases () =
 [<Theory>]
 [<MemberData("getTestCases")>]
 let integrationTest (testDir: string) =
-    printfn "%s" testDir
+    let mainFile = Path.Combine(testDir, mainFileName)
+    let outputFile = Path.Combine(testDir, outputFileName)
+    Assert.True (File.Exists mainFile)
+    Assert.True (File.Exists outputFile)
     ()
