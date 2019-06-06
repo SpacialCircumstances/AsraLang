cce62cf8bdfaa51ea32646061291b4f8e1f63b67
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-06-06 21:49:22 +0200

    Add error positions

--- AsraLang/Typechecker.fs
+++ AsraLang/Typechecker.fs
@@ -68,11 +68,11 @@ let rec typeExpr (state: State) (expr: UntypedExpression): TypedExpression optio
                                                                     | true ->
                                                                         Some (at.varName, te), ctx, errors
                                                                     | false ->
-                                                                        None, ctx, (sprintf "Annotated type: %A, but inferred: %A" tp infT |> TypeError) :: errors
+                                                                        None, ctx, (sprintf "%s: Annotated type: %A, but inferred: %A" (formatPosition def.varData) tp infT |> TypeError) :: errors
                                                             | None ->
                                                                 None, ctx, errors
                                                     | None ->
-                                                        None, state.context, [sprintf "Undefined type: %A" at.typeName |> TypeError]
+                                                        None, state.context, [sprintf "%s: Undefined type: %A" (formatPosition def.varData) at.typeName |> TypeError]
             match name with
                 | None ->
                     None, state, errors
