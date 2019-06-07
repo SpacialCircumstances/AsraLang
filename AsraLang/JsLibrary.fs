d2306b33a72c2310dfc06bf50ed68b7669526615
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-06-07 18:16:42 +0200

    Attempt for generic if

--- AsraLang/JsLibrary.fs
+++ AsraLang/JsLibrary.fs
@@ -12,14 +12,9 @@ let externs =
         { asraName = "/"; asraType = genFunType [ aint; aint ] (aint); externName = "divide" }
         { asraName = "if"; asraType = genFunType [
             genFunType [ aunit ] (abool)
-            genFunType [ aunit ] (aunit)
-            genFunType [ aunit ] (aunit)
+            genFunType [ aunit ] (Generic "a")
+            genFunType [ aunit ] (Generic "a")
         ] (aunit); externName = "iffn" }
-        { asraName = "ifi"; asraType = genFunType [
-            genFunType [ aunit ] (abool)
-            genFunType [ aunit ] (aint)
-            genFunType [ aunit ] (aint)
-        ] (aint); externName = "iffn" } //Bad hack until we have generics
         { asraName = "=="; asraType = genFunType [ aint; aint ] (abool); externName = "eq" }
         { asraName = "!="; asraType = genFunType [ aint; aint ] (abool); externName = "neq" }
         { asraName = "not"; asraType = genFunType [ abool ] (abool); externName = "not" }
