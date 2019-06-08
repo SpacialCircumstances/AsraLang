d680e154350bb81fe3b53fcb1c67e87e6c1c253d
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-06-08 13:03:47 +0200

    Implement more test cases for generics

--- AsraLang.Test/Generics.fs
+++ AsraLang.Test/Generics.fs
@@ -32,3 +32,87 @@ let ``Generic 4`` () =
     let pt = genFunType [ astring ] (genFunType [ astring; astring ] abool)
     let res = genericUnification ft pt
     assertEqResult astring res
+
+let ``Generic 5`` () =
+    let ft = genFunType [ 
+        astring
+        (genFunType [ Generic "a"; Generic "a" ] abool) ] astring
+    let pt = genFunType [ astring ] (genFunType [ Generic "b"; astring ] abool)
+    let res = genericUnification ft pt
+    assertEqResult astring res
+
+let ``Generic 6`` () =
+    let ft = genFunType [ 
+        astring
+        (genFunType [ Generic "a"; Generic "a" ] abool) ] astring
+    let pt = genFunType [ astring ] (genFunType [ aint; astring ] abool)
+    let res = genericUnification ft pt
+    assertError res
+
+let ``Generic 7`` () =
+    let ft = genFunType [ Generic "a"; Generic "a" ] abool
+    let pt = genFunType [ astring ] (Generic "b")
+    let res = genericUnification ft pt
+    assertError res
+
+let ``Generic 8`` () =
+    let p = genFunType [ astring ] (Generic "a")
+    let ft = genFunType [ p; p ] abool
+    let pt = genFunType [
+        genFunType [ astring ] astring ] (genFunType [ astring ] (Generic "b"))
+    let res = genericUnification ft pt
+    assertEqResult abool res
+
+let ``Generic 9`` () =
+    let ft = genFunType [
+        abool
+        genFunType [ aunit ] (Generic "a")
+        genFunType [ aunit ] (Generic "a") ] (Generic "a")
+    let p1 = genFunType [ aunit ] astring
+    let pt = genFunType [ abool; p1 ] p1
+    let res = genericUnification ft pt
+    assertEqResult astring res
+
+let ``Generic 10`` () =
+    let ft = genFunType [
+        abool
+        genFunType [ aunit ] (Generic "a")
+        genFunType [ aunit ] (Generic "a") ] (Generic "a")
+    let pt = genFunType [ 
+        abool
+        (genFunType [ aunit ] (Generic "b")) ] (genFunType [ aunit ] astring)
+    let res = genericUnification ft pt
+    assertEqResult astring res
+
+let ``Generic 11`` () =
+    let ft = genFunType [
+        abool
+        genFunType [ aunit ] (Generic "a")
+        genFunType [ aunit ] (Generic "a") ] (Generic "a")
+    let pt = genFunType [
+        abool
+        genFunType [ aunit ] (Generic "b") ] (genFunType [ aunit ] (Generic "b"))
+    let res = genericUnification ft pt
+    assertEqResult (Generic "b") res
+
+let ``Generic 12`` () =
+    let ft = genFunType [
+        abool
+        genFunType [ aunit ] (Generic "a")
+        genFunType [ aunit ] (Generic "a") ] (Generic "a")
+    let pt = genFunType [ 
+        abool
+        (genFunType [ aunit ] astring) ] (genFunType [ aunit ] aint)
+    let res = genericUnification ft pt
+    assertError res
+
+let ``Generic 13`` () =
+    let ft = genFunType [
+        abool
+        genFunType [ aunit ] (Generic "a")
+        genFunType [ aunit ] (Generic "b") ] (Generic "a")
+    let pt = genFunType [ 
+        abool
+        (genFunType [ aunit ] astring) ] (genFunType [ aunit ] aint)
+    let res = genericUnification ft pt
+    assertEqResult astring res
